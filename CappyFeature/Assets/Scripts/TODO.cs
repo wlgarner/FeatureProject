8fd8f0d0b9381cd334b6a3245547d4ec63ccb07b
Bill <116661796+wlgarner@users.noreply.github.com>
2023-11-09 13:51:06 -0800

    Initial Commit
    Feature Project for CAGD 280.
    I will attempt to make a Cappy feature
    from Mario Odyssey.

---
+++ CappyFeature/Assets/Scripts/TODO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TODO : MonoBehaviour
+{
+    /*
+     * TODO List: Cappy Feature for final project in CAGD280
+     * 1.
+     */
+}
